b7cbcd21bebb8b6a073d4a846860f4dd3db932e8
hfpp2012 <hfpp2012@gmail.com>
2022-01-22 21:23:59 +0800

    add admin layout and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 require 'sidekiq/web'
 Rails.application.routes.draw do
-  mount RailsAdmin::Engine => '/rails_admin', as: 'rails_admin'
   root 'static_pages#home'
+
+  mount RailsAdmin::Engine => '/rails_admin', as: 'rails_admin'
   mount Sidekiq::Web => '/sidekiq'
   mount PgHero::Engine, at: 'pghero'
+
+  draw(:admin)
+
   %w(404 422 500).each do |code|
     get code, to: 'errors#show', code: code
   end
---
+++ config/routes/admin.rb
@@ -0,0 +1,3 @@
+namespace :admin do
+  root 'dashboard#index'
+end

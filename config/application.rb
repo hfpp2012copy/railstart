7bc86f317fd2e947c6fe31c1bde2684950fd3fc5
hfpp2012 <hfpp2012@gmail.com>
2022-01-22 19:48:47 +0800

    custom error page

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module Railstart
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
     config.active_job.queue_adapter = :sidekiq
+    config.exceptions_app = self.routes
 
     # Configuration for the application, engines, and railties goes here.
     #
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 require 'sidekiq/web'
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/rails_admin', as: 'rails_admin'
-  root to: 'static_pages#home'
+  root 'static_pages#home'
   mount Sidekiq::Web => '/sidekiq'
   mount PgHero::Engine, at: 'pghero'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  %w(404 422 500).each do |code|
+    get code, to: 'errors#show', code: code
+  end
 end

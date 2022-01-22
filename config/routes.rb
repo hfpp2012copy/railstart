ef4659e4549bdcf4c682f570b1dc0723b0bf18a0
hfpp2012 <hfpp2012@gmail.com>
2022-01-22 14:23:14 +0800

    add static controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 require 'sidekiq/web'
 Rails.application.routes.draw do
+  root to: 'static_pages#home'
   mount Sidekiq::Web => '/sidekiq'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 

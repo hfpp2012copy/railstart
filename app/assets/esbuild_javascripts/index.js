845f5a342b31358e64f3feffc9031e9376fca717
hfpp2012 <hfpp2012@gmail.com>
2022-01-23 22:49:19 +0800

    using esbuild to load simple data table js

--- app/assets/esbuild_javascripts/index.js
+++ app/assets/esbuild_javascripts/index.js
@@ -1,3 +1,21 @@
 // require("bootstrap/dist/js/bootstrap.bundle");
 // import jQuery from "jquery";
 // window.$ = window.jQuery = jQuery;
+
+import { DataTable } from "simple-datatables";
+const select = (el, all = false) => {
+  el = el.trim();
+  if (all) {
+    return [...document.querySelectorAll(el)];
+  } else {
+    return document.querySelector(el);
+  }
+};
+
+/**
+ * Initiate Datatables
+ */
+const datatables = select(".datatable", true);
+datatables.forEach((datatable) => {
+  new DataTable(datatable);
+});

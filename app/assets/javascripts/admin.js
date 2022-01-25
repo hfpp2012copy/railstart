5b90951346a4de2d8da447d98112af40cd6315a0
hfpp2012 <hfpp2012@gmail.com>
2022-01-25 19:34:23 +0800

    using assets pipeline on same file is bettr

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,2 +1,30 @@
 //= require bootstrap/dist/js/bootstrap.bundle
+//= require simple-datatables/dist/umd/simple-datatables
 //= require niceadmin
+
+// const select = (el, all = false) => {
+//   el = el.trim();
+//   if (all) {
+//     return [...document.querySelectorAll(el)];
+//   } else {
+//     return document.querySelector(el);
+//   }
+// };
+//
+// /**
+//  * Initiate Datatables
+//  */
+// function initSimpleDataTable() {
+//   const datatables = select(".datatable", true);
+//   datatables.forEach((datatable) => {
+//     new simpleDatatables.DataTable(datatable);
+//   });
+// }
+//
+// document.addEventListener("turbo:load", () => {
+//   initSimpleDataTable();
+// });
+//
+// document.addEventListener("DOMContentLoaded", () => {
+//   initSimpleDataTable();
+// });

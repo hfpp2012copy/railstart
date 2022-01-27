763cfedcb7ad172b0453c2d4292445d3119154e3
hfpp2012 <hfpp2012@gmail.com>
2022-01-27 14:26:28 +0800

    fix devise tubo stream superclass reload error

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,38 +1,17 @@
 # frozen_string_literal: true
 
 # https://gorails.com/episodes/devise-hotwire-turbo
-Rails.application.reloader.to_prepare do
-  class TurboFailureApp < Devise::FailureApp
-    def respond
-      if request_format == :turbo_stream
-        redirect
-      else
-        super
-      end
-    end
-
-    def skip_format?
-      %w(html turbo_stream */*).include? request_format.to_s
+class TurboFailureApp < Devise::FailureApp
+  def respond
+    if request_format == :turbo_stream
+      redirect
+    else
+      super
     end
   end
 
-  class TurboController < ApplicationController
-    class Responder < ActionController::Responder
-      def to_turbo_stream
-        controller.render(options.merge(formats: :html))
-      rescue ActionView::MissingTemplate => error
-        if get?
-          raise error
-        elsif has_errors? && default_action
-          render rendering_options.merge(formats: :html, status: :unprocessable_entity)
-        else
-          redirect_to navigation_location
-        end
-      end
-    end
-
-    self.responder = Responder
-    respond_to :html, :turbo_stream
+  def skip_format?
+    %w(html turbo_stream */*).include? request_format.to_s
   end
 end
 
@@ -55,7 +34,7 @@ Devise.setup do |config|
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
   # config.parent_controller = 'DeviseController'
-  config.parent_controller = 'TurboController'
+  config.parent_controller = 'Users::DeviseController'
 
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
